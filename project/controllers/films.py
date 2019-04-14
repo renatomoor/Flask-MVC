45709a8397e5d0a6b8def8bb5bcd79019cde534c
renatomoor <renato.moor@liip.ch>
2019-04-14 16:12:37 +0200

    Create edit film genres

--- project/controllers/films.py
+++ project/controllers/films.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, request, redirect, url_for
 from project import app
 from project.models import films, genres
 
@@ -11,3 +11,22 @@ def films_list():
         film['genres'] = genres.get_genres_by_film_id(film['id_film'])
 
     return render_template('films/films.html', films=films_data)
+
+
+@app.route('/film/<film_id>/genres/edit', methods=['GET', 'POST'])
+def edit_genres_from_film(film_id):
+    film_data = films.get_film_by_id(film_id)
+    film_genres_data = genres.get_genres_by_film_id(film_id)
+    all_genres = genres.get_all_genres()
+
+    for genre in all_genres:
+        if genre in film_genres_data:
+            genre['selected'] = 'selected'
+
+    if request.method == 'POST':
+        film_genres_selected = request.form.getlist('genres[]')
+        films.update_genres_from_film(film_id, film_genres_selected)
+        flash('Les genres du ' + film_data['nom_film'] + ' ont été modifiés')
+        return redirect(url_for('films_list'), code=302)
+
+    return render_template('films/edit_film_genres.html', film=film_data, genres=all_genres)
--- project/models/films.py
+++ project/models/films.py
@@ -5,3 +5,49 @@ def get_all_films():
     sql = "SELECT * FROM t_film"
     films = query(sql, fetch='all')
     return films
+
+
+def get_film_by_id(film_id):
+    sql = "SELECT * FROM t_film WHERE id_film = %s"
+    film = query(sql, fetch='one', values=film_id)
+    return film
+
+
+def add_genre_to_film(film_id, genre_id):
+    sql = "INSERT INTO t_genre_film (`fk_genre`, `fk_film`) VALUES (%s, %s)"
+    values = genre_id, film_id
+    return query(sql, values=values)
+
+
+def remove_genre_from_film(film_id, genre_id):
+    sql = "DELETE FROM t_genre_film WHERE fk_film = %s AND fk_genre = %s;"
+    values = film_id, genre_id
+    return query(sql, values=values)
+
+
+def get_genres_ids(id_film):
+    sql = "SELECT g.id_genre " \
+          "FROM t_genres AS g " \
+          "LEFT JOIN t_genre_film AS tgf ON g.id_genre = tgf.fk_genre " \
+          "LEFT JOIN t_film  AS f ON tgf.fk_film = f.id_film " \
+          "WHERE id_film = %s;"
+
+    genres = query(sql, fetch='all', values=id_film)
+
+    ids_genres = []
+    for genre in genres:
+        ids_genres.append(str(genre['id_genre']))
+
+    return ids_genres
+
+
+def update_genres_from_film(film_id, new_genres_ids):
+    old_genres_ids = get_genres_ids(film_id)
+
+    remove_genres = list(set(old_genres_ids) - set(new_genres_ids))
+    for id_genre in remove_genres:
+        remove_genre_from_film(film_id, id_genre)
+
+    add_genres = list(set(new_genres_ids) - set(old_genres_ids))
+    for id_genre in add_genres:
+        add_genre_to_film(film_id, id_genre)
