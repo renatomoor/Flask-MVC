a6db65e97fbeb8647935f3f7759c8eec56c9e033
renatomoor <renato.moor@liip.ch>
2019-04-15 22:17:32 +0200

    Simplify update_genres_from_film

--- project/models/films.py
+++ project/models/films.py
@@ -1,5 +1,4 @@
 from project.database.connection import query
-from ..models import genres
 
 
 def get_all_films():
@@ -26,18 +25,6 @@ def remove_all_genres_from_film(id_film):
 
 
 def update_genres_from_film(id_film, new_genres_ids):
-    old_genres = genres.get_genres_by_film_id(id_film)
-    old_genres = list(map(get_genre_id_from_genre, old_genres))
-
-    add_genres = list(set(new_genres_ids) - set(old_genres))
-
-    for id_genre in add_genres:
+    remove_all_genres_from_film(id_film)
+    for id_genre in new_genres_ids:
         add_genre_to_film(id_film, id_genre)
-
-    remove_genres = list(set(old_genres) - set(new_genres_ids))
-    for id_genre in remove_genres:
-        remove_genre_from_film(id_film, id_genre)
-
-
-def get_genre_id_from_genre(genre):
-    return genre['id_genre']
